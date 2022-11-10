1ae970a089b09ca5b835ff3aae91671de9b4c9d4
Santiago Arias <91507928+santiarias65@users.noreply.github.com>
2022-11-09 19:10:47 -0500

    Update SistemaExamenesApplication.java

--- classhoot-backend/src/main/java/com/sistemasexamenes/sistemaexamenes/SistemaExamenesApplication.java
+++ classhoot-backend/src/main/java/com/sistemasexamenes/sistemaexamenes/SistemaExamenesApplication.java
@@ -24,27 +24,6 @@ public class SistemaExamenesApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /*Usuario usuario = new Usuario();
-        usuario.setNombre("Santiago");
-        usuario.setApellido("Arias");
-        usuario.setUsername("santiarias");
-        usuario.setPassword("123");
-        usuario.setEmail("santiarias@gmail.com");
-        usuario.setTelefono("301735243");
-        usuario.setPerfil("foto.png");
-
-        Rol rol = new Rol();
-        rol.setRolId(1L);
-        rol.setNombre("ADMIN");
-
-        Set<UsuarioRol> usuarioRoles = new HashSet<>();
-        UsuarioRol usuarioRol = new UsuarioRol();
-        usuarioRol.setRol(rol);
-        usuarioRol.setUsuario(usuario);
-
-        usuarioRoles.add(usuarioRol);
-
-        Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario,usuarioRoles);
-        System.out.println(usuarioGuardado.getUsername());*/
+        
     }
 }
